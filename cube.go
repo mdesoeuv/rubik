4051f2f759ef4ada4ccb2be60c6919350c7f017e
DoomDuck <docdoomduck@protonmail.com>
2024-06-30 19:52:36 +0200

    feat(cube): maybe half working `apply(Move)`

--- cube.go
+++ cube.go
@@ -7,14 +7,15 @@ import (
 )
 
 type Color = int
+
 const (
-	Red Color = 0
-	Blue Color = iota
-	Green Color = iota
-	White Color = iota
-	Yellow Color = iota
-	Orange Color = iota
-	ColorCount int = iota
+	Red        Color = 0
+	Blue       Color = iota
+	Green      Color = iota
+	White      Color = iota
+	Yellow     Color = iota
+	Orange     Color = iota
+	ColorCount int   = iota
 )
 
 type Cube struct {
@@ -41,7 +42,7 @@ func NewCubeSolved() *Cube {
 	return &cube
 }
 
-func (c *Cube)IsSolved() bool {
+func (c *Cube) IsSolved() bool {
 	for side, face := range c.faces {
 		if !FaceIsUniform(face, side) {
 			return false
@@ -50,54 +51,165 @@ func (c *Cube)IsSolved() bool {
 	return true
 }
 
-type Coord struct {
-	line int
+type FaceCoord struct {
+	line   int
 	column int
+}
+
+type CubeCoord struct {
+	side  Side
+	coord FaceCoord
 }
 
 func rotateFace(face Face, clockWise bool) (result Face) {
 	// Center never moves
 	result.f[1][1] = face.f[1][1]
 
+	cycle := []FaceCoord{
+		{0, 0},
+		{0, 1},
+		{0, 2},
+		{1, 2},
+		{2, 2},
+		{2, 1},
+		{2, 0},
+	}
+
 	if clockWise {
-		result.f[0][0] = face.f[0][1]
-		result.f[0][1] = face.f[0][2]
-		result.f[0][2] = face.f[1][2]
-		result.f[1][2] = face.f[2][2]
-		result.f[2][2] = face.f[2][1]
-		result.f[2][1] = face.f[2][0]
-		result.f[2][0] = face.f[1][0]
-		result.f[1][0] = face.f[0][0]
+		for i := range cycle {
+			to, from := cycle[i], cycle[(i+2)%len(cycle)]
+			result.f[to.line][to.column] = face.f[from.line][from.column]
+		}
 	} else {
-		result.f[0][0] = face.f[0][0]
-		result.f[0][1] = face.f[0][1]
-		result.f[0][2] = face.f[0][2]
-		result.f[1][2] = face.f[1][2]
-		result.f[2][2] = face.f[2][2]
-		result.f[2][1] = face.f[2][1]
-		result.f[2][0] = face.f[2][0]
-		result.f[1][0] = face.f[0][0]
+		for i := range cycle {
+			from, to := cycle[i], cycle[(i+2)%len(cycle)]
+			result.f[to.line][to.column] = face.f[from.line][from.column]
+		}
 	}
 	return
 }
 
-func (c *Cube)apply(move Move) {
+func rotateCrown(cube *Cube, move Move) {
+	crowns := map[Side][12]CubeCoord{
+		Front: {
+			{side: Up, coord: FaceCoord{line: 2, column: 0}},
+			{side: Up, coord: FaceCoord{line: 2, column: 1}},
+			{side: Up, coord: FaceCoord{line: 2, column: 2}},
+			{side: Right, coord: FaceCoord{line: 0, column: 0}},
+			{side: Right, coord: FaceCoord{line: 1, column: 0}},
+			{side: Right, coord: FaceCoord{line: 2, column: 0}},
+			{side: Down, coord: FaceCoord{line: 0, column: 0}},
+			{side: Down, coord: FaceCoord{line: 0, column: 1}},
+			{side: Down, coord: FaceCoord{line: 0, column: 2}},
+			{side: Left, coord: FaceCoord{line: 2, column: 2}},
+			{side: Left, coord: FaceCoord{line: 1, column: 2}},
+			{side: Left, coord: FaceCoord{line: 0, column: 2}},
+		},
+		Up: {
+			{side: Back, coord: FaceCoord{line: 0, column: 2}},
+			{side: Back, coord: FaceCoord{line: 0, column: 1}},
+			{side: Back, coord: FaceCoord{line: 0, column: 0}},
+			{side: Right, coord: FaceCoord{line: 0, column: 2}},
+			{side: Right, coord: FaceCoord{line: 0, column: 1}},
+			{side: Right, coord: FaceCoord{line: 0, column: 0}},
+			{side: Front, coord: FaceCoord{line: 0, column: 2}},
+			{side: Front, coord: FaceCoord{line: 0, column: 1}},
+			{side: Front, coord: FaceCoord{line: 0, column: 0}},
+			{side: Left, coord: FaceCoord{line: 0, column: 2}},
+			{side: Left, coord: FaceCoord{line: 0, column: 1}},
+			{side: Left, coord: FaceCoord{line: 0, column: 0}},
+		},
+		Right: {
+			{side: Up, coord: FaceCoord{line: 2, column: 2}},
+			{side: Up, coord: FaceCoord{line: 1, column: 2}},
+			{side: Up, coord: FaceCoord{line: 0, column: 2}},
+			{side: Back, coord: FaceCoord{line: 0, column: 0}},
+			{side: Back, coord: FaceCoord{line: 1, column: 0}},
+			{side: Back, coord: FaceCoord{line: 2, column: 0}},
+			{side: Down, coord: FaceCoord{line: 2, column: 0}},
+			{side: Down, coord: FaceCoord{line: 1, column: 0}},
+			{side: Down, coord: FaceCoord{line: 0, column: 0}},
+			{side: Front, coord: FaceCoord{line: 2, column: 2}},
+			{side: Front, coord: FaceCoord{line: 1, column: 2}},
+			{side: Front, coord: FaceCoord{line: 0, column: 2}},
+		},
+		Down: {
+			{side: Front, coord: FaceCoord{line: 2, column: 2}},
+			{side: Front, coord: FaceCoord{line: 2, column: 1}},
+			{side: Front, coord: FaceCoord{line: 2, column: 0}},
+			{side: Left, coord: FaceCoord{line: 2, column: 2}},
+			{side: Left, coord: FaceCoord{line: 2, column: 1}},
+			{side: Left, coord: FaceCoord{line: 2, column: 0}},
+			{side: Back, coord: FaceCoord{line: 2, column: 2}},
+			{side: Back, coord: FaceCoord{line: 2, column: 1}},
+			{side: Back, coord: FaceCoord{line: 2, column: 0}},
+			{side: Right, coord: FaceCoord{line: 2, column: 2}},
+			{side: Right, coord: FaceCoord{line: 2, column: 1}},
+			{side: Right, coord: FaceCoord{line: 2, column: 0}},
+		},
+		Left: {
+			{side: Up, coord: FaceCoord{line: 0, column: 0}},
+			{side: Up, coord: FaceCoord{line: 1, column: 0}},
+			{side: Up, coord: FaceCoord{line: 2, column: 0}},
+			{side: Front, coord: FaceCoord{line: 0, column: 0}},
+			{side: Front, coord: FaceCoord{line: 1, column: 0}},
+			{side: Front, coord: FaceCoord{line: 2, column: 0}},
+			{side: Down, coord: FaceCoord{line: 0, column: 2}},
+			{side: Down, coord: FaceCoord{line: 1, column: 2}},
+			{side: Down, coord: FaceCoord{line: 2, column: 2}},
+			{side: Back, coord: FaceCoord{line: 2, column: 2}},
+			{side: Back, coord: FaceCoord{line: 1, column: 2}},
+			{side: Back, coord: FaceCoord{line: 0, column: 2}},
+		},
+		Back: {
+			{side: Up, coord: FaceCoord{line: 0, column: 2}},
+			{side: Up, coord: FaceCoord{line: 0, column: 1}},
+			{side: Up, coord: FaceCoord{line: 0, column: 0}},
+			{side: Left, coord: FaceCoord{line: 0, column: 0}},
+			{side: Left, coord: FaceCoord{line: 1, column: 0}},
+			{side: Left, coord: FaceCoord{line: 2, column: 0}},
+			{side: Down, coord: FaceCoord{line: 2, column: 2}},
+			{side: Down, coord: FaceCoord{line: 2, column: 1}},
+			{side: Down, coord: FaceCoord{line: 2, column: 0}},
+			{side: Right, coord: FaceCoord{line: 2, column: 2}},
+			{side: Right, coord: FaceCoord{line: 1, column: 2}},
+			{side: Right, coord: FaceCoord{line: 0, column: 2}},
+		},
+	}
+
+	crown := crowns[move.Side]
+
+	newCube := *cube
+
+	for i := range crown {
+		from, to := crown[i], crown[(i+3)%len(crown)]
+		*newCube.get(to) = *cube.get(from)
+	}
+
+	*cube = newCube
+}
+
+func (c *Cube) get(coord CubeCoord) *Side {
+	return &c.faces[coord.side].f[coord.coord.line][coord.coord.column]
+}
+
+func (c *Cube) apply(move Move) {
 	face := &c.faces[move.Side]
 
 	// rotate face it self
 	*face = rotateFace(*face, move.Clockwise)
 
-	// TODO: rotate crown
+	rotateCrown(c, move)
 	return
 }
 
-func (c *Cube)isSolved() bool {
+func (c *Cube) isSolved() bool {
 	seenColors := [ColorCount]bool{}
 	for _, face := range c.faces {
 		var currentColor *Color = nil
 		for _, line := range face.f {
 			for _, color := range line {
-				if currentColor != nil  && color != *currentColor {
+				if currentColor != nil && color != *currentColor {
 					return false
 				}
 				currentColor = &color
@@ -111,8 +223,8 @@ func (c *Cube)isSolved() bool {
 	return true
 }
 
-func (c *Cube)print() {
-	
+func (c *Cube) print() {
+
 	lines := [9]string{}
 	emptyLine := "         "
 	lines[0] = emptyLine + " " + c.faces[Up].FaceGetLineString(0)
@@ -127,9 +239,13 @@ func (c *Cube)print() {
 	lines[7] = emptyLine + " " + c.faces[Down].FaceGetLineString(1)
 	lines[8] = emptyLine + " " + c.faces[Down].FaceGetLineString(2)
 
+	lines[6] = emptyLine + c.faces[Down].FaceGetLineString(0) + emptyLine + emptyLine
+	lines[7] = emptyLine + c.faces[Down].FaceGetLineString(1) + emptyLine + emptyLine
+	lines[8] = emptyLine + c.faces[Down].FaceGetLineString(2) + emptyLine + emptyLine
+
 	for _, line := range lines {
 		fmt.Println(line)
-	} 
+	}
 }
 
 var theme = map[Side]func(a ...interface{}) string{
