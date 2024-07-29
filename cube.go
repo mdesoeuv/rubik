6d0f0ad2dcdbbe12d0a8e87f6b4c9e7b07dd394a
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-07-29 11:32:01 +0200

    fix: removed unnecessary empty faces

--- cube.go
+++ cube.go
@@ -130,18 +130,18 @@ func (c *Cube)isSolved() bool {
 func (c *Cube)print() {
 	
 	lines := [9]string{}
-	emptyLine := "          "
-	lines[0] = emptyLine + c.faces[Up].FaceGetLineString(0) + emptyLine + emptyLine
-	lines[1] = emptyLine + c.faces[Up].FaceGetLineString(1) + emptyLine + emptyLine
-	lines[2] = emptyLine + c.faces[Up].FaceGetLineString(2) + emptyLine + emptyLine + "\n"
+	emptyLine := "         "
+	lines[0] = emptyLine + " " + c.faces[Up].FaceGetLineString(0)
+	lines[1] = emptyLine + " " + c.faces[Up].FaceGetLineString(1)
+	lines[2] = emptyLine + " " + c.faces[Up].FaceGetLineString(2) + "\n"
 
 	lines[3] = c.faces[Left].FaceGetLineString(0) + " " + c.faces[Front].FaceGetLineString(0) + " " + c.faces[Right].FaceGetLineString(0) + " " + c.faces[Back].FaceGetLineString(0)
 	lines[4] = c.faces[Left].FaceGetLineString(1) + " " + c.faces[Front].FaceGetLineString(1) + " " + c.faces[Right].FaceGetLineString(1) + " " + c.faces[Back].FaceGetLineString(1)
 	lines[5] = c.faces[Left].FaceGetLineString(2) + " " + c.faces[Front].FaceGetLineString(2) + " " + c.faces[Right].FaceGetLineString(2) + " " + c.faces[Back].FaceGetLineString(2) + "\n"
 	
-	lines[6] = emptyLine + c.faces[Down].FaceGetLineString(0) + emptyLine + emptyLine
-	lines[7] = emptyLine + c.faces[Down].FaceGetLineString(1) + emptyLine + emptyLine
-	lines[8] = emptyLine + c.faces[Down].FaceGetLineString(2) + emptyLine + emptyLine
+	lines[6] = emptyLine + " " + c.faces[Down].FaceGetLineString(0)
+	lines[7] = emptyLine + " " + c.faces[Down].FaceGetLineString(1)
+	lines[8] = emptyLine + " " + c.faces[Down].FaceGetLineString(2)
 
 	for _, line := range lines {
 		fmt.Println(line)
