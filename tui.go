c830a7205c73ee17569339e196c957b14e50e9a5
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-08-02 13:45:24 +0200

    refactor(tui): edit and explore menu structures

--- tui.go
+++ tui.go
@@ -13,19 +13,27 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type model struct {
-	cube        Cube
-	initialCube Cube
-	solution    SolutionMsg
-	list        list.Model
-	spinner     spinner.Model
-	stopwatch   stopwatch.Model
-	keymap      keymap
-	help        help.Model
-	isSolving   bool
-	isExploring bool
+type ExploreMenu struct {
 	lastMove    string
 	lastIndex   int
+	cube        Cube
+	solution    SolutionMsg
+	isExploring bool
+}
+
+type EditMenu struct {
+	spinner   spinner.Model
+	stopwatch stopwatch.Model
+	isSolving bool
+}
+
+type model struct {
+	displayedCube Cube
+	menu          list.Model
+	edit          EditMenu
+	explore       ExploreMenu
+	keymap        keymap
+	help          help.Model
 }
 
 func initialModel(c *Cube) model {
@@ -35,18 +43,27 @@ func initialModel(c *Cube) model {
 
 	cubeCopy := *c
 
-	return model{
-		cube:        cubeCopy,
-		initialCube: *c,
-		list:        CreateApplyMoveList(),
-		spinner:     s,
-		stopwatch:   stopwatch.NewWithInterval(time.Millisecond),
-		keymap:      NewKeyMap(),
-		help:        help.New(),
-		isSolving:   false,
-		isExploring: false,
+	editMenu := EditMenu{
+		spinner:   s,
+		stopwatch: stopwatch.NewWithInterval(time.Millisecond),
+		isSolving: false,
+	}
+
+	exploreMenu := ExploreMenu{
 		lastMove:    "Start",
 		lastIndex:   0,
+		cube:        cubeCopy,
+		solution:    SolutionMsg{},
+		isExploring: false,
+	}
+
+	return model{
+		displayedCube: cubeCopy,
+		menu:          CreateApplyMoveList(),
+		edit:          editMenu,
+		explore:       exploreMenu,
+		keymap:        NewKeyMap(),
+		help:          help.New(),
 	}
 }
 
@@ -75,25 +92,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var stopWatchCmd tea.Cmd
 	var spinnerCmd tea.Cmd
 	var listCmd tea.Cmd
-	m.stopwatch, stopWatchCmd = m.stopwatch.Update(msg)
-	m.spinner, spinnerCmd = m.spinner.Update(msg)
-	m.list, listCmd = m.list.Update(msg)
+	m.edit.stopwatch, stopWatchCmd = m.edit.stopwatch.Update(msg)
+	m.edit.spinner, spinnerCmd = m.edit.spinner.Update(msg)
+	m.menu, listCmd = m.menu.Update(msg)
 
 	var myCmd tea.Cmd
 	switch msg := msg.(type) {
 
 	case tea.WindowSizeMsg:
-		m.list.SetWidth(msg.Width)
+		m.menu.SetWidth(msg.Width)
 		return m, nil
 
 	case SolutionMsg:
-		m.isSolving = false
+		m.edit.isSolving = false
 		m.keymap.solve.SetEnabled(true)
 		m.keymap.reset.SetEnabled(true)
 		m.keymap.explore.SetEnabled(true)
 
-		stopWatchCmd = m.stopwatch.Stop()
-		m.solution = msg
+		stopWatchCmd = m.edit.stopwatch.Stop()
+		m.explore.solution = msg
 
 	case tea.KeyMsg:
 
@@ -103,34 +120,34 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			myCmd = tea.Quit
 
 		case key.Matches(msg, m.keymap.right):
-			i, ok := m.list.SelectedItem().(item)
+			i, ok := m.menu.SelectedItem().(item)
 			if ok {
 				if len(i) == 1 {
-					m.list.SetItem(m.list.Index(), item(string(i)+"2"))
+					m.menu.SetItem(m.menu.Index(), item(string(i)+"2"))
 				} else if i[1] == '\'' {
-					m.list.SetItem(m.list.Index(), item(string(i[0])))
+					m.menu.SetItem(m.menu.Index(), item(string(i[0])))
 				}
 
 			}
 
 		case key.Matches(msg, m.keymap.left):
-			i, ok := m.list.SelectedItem().(item)
+			i, ok := m.menu.SelectedItem().(item)
 			if ok {
 				if len(i) == 1 {
-					m.list.SetItem(m.list.Index(), item(string(i)+"'"))
+					m.menu.SetItem(m.menu.Index(), item(string(i)+"'"))
 				} else if i[1] == '2' {
-					m.list.SetItem(m.list.Index(), item(string(i[0])))
+					m.menu.SetItem(m.menu.Index(), item(string(i[0])))
 				}
 			}
 
 		case key.Matches(msg, m.keymap.solve):
-			cube := m.cube
-			m.isSolving = true
+			cube := m.displayedCube
+			m.edit.isSolving = true
 			m.keymap.solve.SetEnabled(false)
 			m.keymap.reset.SetEnabled(false)
 			myCmd = tea.Batch(
-				m.stopwatch.Start(),
-				m.spinner.Tick,
+				m.edit.stopwatch.Start(),
+				m.edit.spinner.Tick,
 				func() tea.Msg {
 					return SolutionMsg{
 						moves: cube.solve(),
@@ -139,7 +156,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			)
 
 		case key.Matches(msg, m.keymap.enter):
-			i, ok := m.list.SelectedItem().(item)
+			i, ok := m.menu.SelectedItem().(item)
 			if ok {
 				choice := string(i)
 				move, err := ParseMove(choice)
@@ -147,59 +164,59 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					// TODO: Better
 					fmt.Println(err)
 				}
-				m.cube.apply(move)
+				m.displayedCube.apply(move)
 				m.keymap.explore.SetEnabled(false)
 			}
 
 		case key.Matches(msg, m.keymap.reset):
-			m.stopwatch.Reset()
-			m.cube = *NewCubeSolved()
-			m.solution = SolutionMsg{}
+			m.edit.stopwatch.Reset()
+			m.displayedCube = *NewCubeSolved()
+			m.explore.solution = SolutionMsg{}
 
 		case key.Matches(msg, m.keymap.explore):
-			m.SwitchKeyBindings(m.isExploring)
-			if !m.isExploring {
-				m.list = CreateExploreMoveList(m.solution.moves)
-				m.initialCube = m.cube
-				m.lastIndex = 0
+			m.SwitchKeyBindings(m.explore.isExploring)
+			if !m.explore.isExploring {
+				m.menu = CreateExploreMoveList(m.explore.solution.moves)
+				m.explore.cube = m.displayedCube
+				m.explore.lastIndex = 0
 			} else {
-				m.list = CreateApplyMoveList()
-				m.cube = m.initialCube
+				m.menu = CreateApplyMoveList()
+				m.displayedCube = m.explore.cube
 			}
-			m.isExploring = !m.isExploring
+			m.explore.isExploring = !m.explore.isExploring
 
 		case key.Matches(msg, m.keymap.down):
-			if m.isExploring {
-				i, ok := m.list.SelectedItem().(item)
+			if m.explore.isExploring {
+				i, ok := m.menu.SelectedItem().(item)
 				// fmt.Println("item number: ", len(m.list.VisibleItems()))
 				move := string(i)
-				if ok && i != "Start" && m.lastIndex != len(m.list.VisibleItems())-1 {
+				if ok && i != "Start" && m.explore.lastIndex != len(m.menu.VisibleItems())-1 {
 					choice := move
 					move, err := ParseMove(choice)
 					if err != nil {
 						// TODO: Better
 						fmt.Println(err)
 					}
-					m.cube.apply(move)
+					m.displayedCube.apply(move)
 				}
-				m.lastMove = move
-				m.lastIndex = m.list.Index()
+				m.explore.lastMove = move
+				m.explore.lastIndex = m.menu.Index()
 			}
 
 		case key.Matches(msg, m.keymap.up):
-			if m.isExploring {
-				i, ok := m.list.SelectedItem().(item)
+			if m.explore.isExploring {
+				i, ok := m.menu.SelectedItem().(item)
 				move := string(i)
-				if ok && m.lastMove != "Start" {
-					move, err := ParseMove(m.lastMove)
+				if ok && m.explore.lastMove != "Start" {
+					move, err := ParseMove(m.explore.lastMove)
 					if err != nil {
 						fmt.Println(err)
 					}
 					move = move.Reverse()
-					m.cube.apply(move)
+					m.displayedCube.apply(move)
 				}
-				m.lastMove = move
-				m.lastIndex = m.list.Index()
+				m.explore.lastMove = move
+				m.explore.lastIndex = m.menu.Index()
 			}
 
 		}
@@ -210,19 +227,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 
-	s := rectangleStyle.Render(m.cube.blueprint()) + "\n"
+	s := rectangleStyle.Render(m.displayedCube.blueprint()) + "\n"
 
-	s += m.list.View()
+	s += m.menu.View()
 
-	if m.isSolving {
-		s += resultStyle.Render("\n" + m.spinner.View() + "Solving..." + fmt.Sprintf(" (%s)", m.stopwatch.View()))
-	} else if m.solution.moves != nil {
+	if m.edit.isSolving {
+		s += resultStyle.Render("\n" + m.edit.spinner.View() + "Solving..." + fmt.Sprintf(" (%s)", m.edit.stopwatch.View()))
+	} else if m.explore.solution.moves != nil {
 		solutionString := "\nSolution found: "
 
-		for _, move := range m.solution.moves {
+		for _, move := range m.explore.solution.moves {
 			solutionString += move.CompactString() + " "
 		}
-		solutionString += fmt.Sprintf("(%s)", m.stopwatch.View())
+		solutionString += fmt.Sprintf("(%s)", m.edit.stopwatch.View())
 		s += resultStyle.Render(solutionString)
 	}
 	s += m.helpView()
