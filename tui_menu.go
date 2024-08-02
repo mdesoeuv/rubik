72703f16591d831c8af5d36af9d6cfd9d3d2c96f
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-08-02 10:59:34 +0200

    feat(tui): new arrow cursor

--- tui_menu.go
+++ tui_menu.go
@@ -29,7 +29,7 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 	fn := itemStyle.Render
 	if index == m.Index() {
 		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+			return selectedItemStyle.Render("➤ " + strings.Join(s, " "))
 		}
 	}
 
