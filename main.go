b25f72d55d375396d2aac12cce692fc0bf859196
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-09-05 10:53:07 +0200

    feat: allow empty move list

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 	cmn "github.com/mdesoeuv/rubik/common"
 	tui "github.com/mdesoeuv/rubik/tui"
 	vc "github.com/mdesoeuv/rubik/visual_cepo"
+	"math/rand/v2"
 )
 
 var (
 	tuiFlag     = flag.Bool("tui", false, "Enable Terminal User Interface")
 	profileFlag = flag.Bool("profile", false, "Enable CPU profiling")
 	verboseFlag = flag.Bool("verbose", false, "Enable verbose output")
+	shuffleFlag = flag.Int("shuffle", 0, "Shuffle the cube with N moves")
 )
 
 func main() {
@@ -26,16 +28,12 @@ func main() {
 		defer stopProfiling()
 	}
 
-	args := flag.Args()
-	if len(args) != 1 && !*tuiFlag {
-		fmt.Println("Usage: go run main.go <move list>")
-		return
-	}
-
 	var (
 		moveListStr = ""
 		moveList    = []cmn.Move{}
 	)
+
+	args := flag.Args()
 	if len(args) > 0 {
 		moveListStr = args[0]
 		moveList, err = cmn.ParseMoveList(moveListStr)
@@ -52,6 +50,15 @@ func main() {
 		cube.Apply(move)
 	}
 	solver := cube.NewSolver()
+
+	if *shuffleFlag > 0 {
+		if *verboseFlag {
+			fmt.Println("Shuffling cube: ", *shuffleFlag)
+		}
+		r := rand.New(rand.NewPCG(rand.Uint64(), rand.Uint64()))
+		cmn.Shuffle(cube, r, *shuffleFlag)
+	}
+
 	if *tuiFlag {
 		p := tea.NewProgram(tui.InitialModel(cube, solvedCube, solver))
 		if _, err := p.Run(); err != nil {
