4cde8edeec77ac0885b341b3bd1eee011b1eb4fb
Dorian Burgun <docdoomduck@protonmail.com>
2024-09-05 11:03:00 +0200

    fix(cepo_test): Remove reference to `MakeG3Cubes` (#67)

--- cepo/g3_test.go
+++ cepo/g3_test.go
@@ -4,16 +4,6 @@ import (
 	"testing"
 )
 
-func TestMakeG3CubesHasSizeFromArticle(t *testing.T) {
-	G3Cubes := MakeG3Cubes()
-
-	foundSize := len(G3Cubes)
-	expectedSize := 663_552
-	if foundSize != expectedSize {
-		t.Fatal("G3Cubes should have size from article")
-	}
-}
-
 func TestMakeG3Table(t *testing.T) {
 	table := MakeG3HeuristicTable()
 
@@ -25,5 +15,10 @@ func TestMakeG3Table(t *testing.T) {
 	}
 	if maxDistance != 15 {
 		t.Errorf("Max distance should be 15 but found: %v", maxDistance)
+	}
+	foundSize := len(table)
+	expectedSize := 663_552
+	if foundSize != expectedSize {
+		t.Fatal("G3Cubes should have size from article")
 	}
 }
